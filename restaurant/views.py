a7b3b71b6e72716ea2cdcd3f99d93c1e142e220e
Mygaverse <myagaverse@gmail.com>
2024-11-25 15:28:42 +0800

    serializers, views, and urls with router

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User, Group
+from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer, UserSerializer
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
 
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
